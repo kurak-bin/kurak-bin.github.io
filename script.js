8cce24b9fe0ff06c51bbcf2cc327c1d450d0ab2f
kurak-bin <165349931+kurak-bin@users.noreply.github.com>
2024-06-19 13:24:24 -0400

    Update script.js
    DOMContentLoaded trigger has troubles with Github pages, will reimplement at a later date.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const numStars = 50; // Number of stars to generate
+const numStars = 50; // Number of stars to generate
 
-    for (let i = 0; i < numStars; i++) {
-        createStar();
-    }
+for (let i = 0; i < numStars; i++) {
+    createStar();
+}
 
-    function createStar() {
-        const star = document.createElement('div');
-        star.className = 'star';
-        star.style.top = `${Math.random() * 100}%`;
-        star.style.left = `${Math.random() * 100}%`;
-        star.style.animationDelay = `${Math.random() * 3}s`; // Randomize animation delay
-        document.body.appendChild(star);
-    }
-});
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 3}s`; // Randomize animation delay
+    document.body.appendChild(star);
+}
