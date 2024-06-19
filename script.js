5f6c460ae38f44ac89679e500e156abe1666401b
kurak-bin <165349931+kurak-bin@users.noreply.github.com>
2024-06-19 13:28:16 -0400

    Attempting to fix JS

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const numStars = 50; // Number of stars to generate
 
-for (let i = 0; i < numStars; i++) {
-    createStar();
-}
-
 function createStar() {
     const star = document.createElement('div');
     star.className = 'star';
@@ -12,3 +8,9 @@ function createStar() {
     star.style.animationDelay = `${Math.random() * 3}s`; // Randomize animation delay
     document.body.appendChild(star);
 }
+
+for (let i = 0; i < numStars; i++) {
+    createStar();
+}
+
+console.log("test");
